11d4001e7cbbdc505f4f52bb96350a7a7be556c2
Cristiano Fernandes <cristianobfernandes@gmail.com>
2012-09-18 19:08:51 -0300

    Organizando o código. Separado em reconhecer rosto e olhos.
    Signed-off-by: Cristiano Fernandes <cristianobfernandes@gmail.com>

--- src/trab1.c
+++ src/trab1.c
@@ -8,33 +8,74 @@
 #include <cv.h>
 #include <highgui.h>
 
-//#define cascade_name "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml"
-#define cascade_name "/usr/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml"
+#define cascade_name "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml"
+#define eye_cascade "/usr/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml"
 
 
-IplImage *image;
 
-void check_eye(CvRect *r) {
+void check_eye(IplImage *img) {
   int i, j;
+  CvHaarClassifierCascade *cascade;
+  CvSeq *eyes;
+  /* 8-bit depth RGB representation of color red */
+  static CvScalar RED = {0, 0, 255};
+  CvPoint ul;
+  CvPoint lr;
+  CvMemStorage *storage;
+  CvRect *r;
+
+
+  /* Load the classifier data from the .xml file */
+  cascade = (CvHaarClassifierCascade *)cvLoad(eye_cascade, NULL, NULL, NULL);
+
+  storage = cvCreateMemStorage(0);
+  cvClearMemStorage(storage);
+  eyes = cvHaarDetectObjects(
+        img,
+        cascade,
+        storage,
+        1.1,
+        2,
+        CV_HAAR_DO_CANNY_PRUNING,
+        cvSize(40, 40),
+        cvSize(40, 40));
+
+  /* used cvHaarDetectObjects */
+
+
+  for (i = 0; i < (eyes ? eyes->total : 0); i++) {
+    r = (CvRect *)cvGetSeqElem(eyes, i);
+    ul.x = r->x;
+    ul.y = r->y;
+    lr.x = r->x + r->width;
+    lr.y = r->y + r->height;
+
+    cvRectangle(img, ul, lr, RED, 3, 8, 0);
+  }
 
    for (i = r->y; i < (r->y + r->height); i++) {
      for (j = r->x; j < (r->x + r->width); j++) {
-       ((uchar*)(image->imageData + i * image->widthStep))[j*3] = 0;
-       ((uchar*)(image->imageData + i * image->widthStep))[j*3+1] = 0;
-       ((uchar*)(image->imageData + i * image->widthStep))[j*3+2] = 0;
+     /*
+       ((uchar*)(img->imageData + i * img->widthStep))[j*3] = 0;
+       ((uchar*)(img->imageData + i * img->widthStep))[j*3+1] = 0;
+       ((uchar*)(img->imageData + i * img->widthStep))[j*3+2] = 0;
+    */
      }
    }
 }
 
 int main(int argc, char *argv[])
 {
+  IplImage *image;
   CvHaarClassifierCascade *cascade;
-  IplImage *img2;
 //  IplImage *gray;
   CvMemStorage *storage;
   CvSeq *faces;
   int i, j;
   CvRect *r;
+  IplImage *faceImage;
+  IplImage *eyesImage;
+
 
   /* Load the classifier data from the .xml file */
   cascade = (CvHaarClassifierCascade *)cvLoad(cascade_name, NULL, NULL, NULL);
@@ -45,19 +86,6 @@ int main(int argc, char *argv[])
   /* read the input image */
   image = cvLoadImage(argv[1], 1);
 
-#if 0
-  /*
-     create a b&w image of the same size object as a placeholder for now
-     - cvSize(width, height) is the object representing dimensions of the image
-     - 8 stands for 8-bit depth
-     - 1 means one-channel (b&w)
-     Hence a 24bit RGB image of size 800x600 would be created with cvCreateImage( cvSize(800, 600), 8, 3);
-   */
-  gray = cvCreateImage(cvSize(image->width, image->height), 8, 1);
-
-  /* now convert the input image into b&w and store it in the placeholder */
-  cvCvtColor(image, gray, CV_BGR2GRAY);
-#endif
   /* create memory storage to be used by cvHaarDetectObjects */
   storage = cvCreateMemStorage(0);
   cvClearMemStorage(storage);
@@ -71,55 +99,34 @@ int main(int argc, char *argv[])
         cvSize(40, 40),
         cvSize(40, 40));
 
-  /* used cvHaarDetectObjects */
-
-  /* 8-bit depth RGB representation of color red */
-  static CvScalar RED = {0, 0, 255};
-
   /* go through all the detected faces, and draw them into the input image */
   for (i = 0; i < (faces ? faces->total : 0); i++) {
     r = (CvRect *)cvGetSeqElem(faces, i);
-    CvPoint ul;
-    CvPoint lr;
-    ul.x = r->x;
-    ul.y = r->y;
-    lr.x = r->x + r->width;
-    lr.y = r->y + r->height;
 
-    check_eye(r);
+    cvSetImageROI(image, *r);
+    faceImage = cvCreateImage(cvGetSize(image), image->depth, image->nChannels);
+    cvCopy(image, faceImage, NULL);
+    cvResetImageROI(image);
 
-   //cvSetImageROI(image, *r);
+    eyesImage = cvCreateImage(cvGetSize(image), image->depth, image->nChannels);
+    cvCopy(image, eyesImage, NULL);
 
-//   cvCopy(image, img2, NULL);
-   //cvResetImageROI(image);
-
-    //IplImage *img2 = cvCreateImage(cvGetSize(image), image->depth, image->nChannels);
-//    break;
-
-    /* draws a rectangle with given coordinates of the upper left
-       and lower right corners into an image */
-    //cvRectangle(image, ul, lr, RED, 3, 8, 0);
+    check_eye(eyesImage);
   }
-  /*
-  for (i = r->y; i < (r->y + r->height); i++) {
-    for (j = r->x; j < (r->x + r->width); j++) {
-      ((uchar*)(image->imageData + i * image->widthStep))[j*3] = 0;
-      ((uchar*)(image->imageData + i * image->widthStep))[j*3+1] = 0;
-      ((uchar*)(image->imageData + i * image->widthStep))[j*3+2] = 0;
-    }
-  }
-  */
-#if 0
-  /* free up the memory */
-  cvReleaseImage(&gray);
-#endif
 
   /* show the result and wait for a keystroke form user before finishing */
-  cvShowImage("result", image);
+  cvShowImage("normal", image);
+  cvShowImage("face", faceImage);
+  cvShowImage("eye", eyesImage);
 
   cvWaitKey(0);
   cvReleaseImage(&image);
-  cvDestroyWindow("result");
+  cvReleaseImage(&faceImage);
+  cvReleaseImage(&eyesImage);
+
+  cvDestroyWindow("normal");
+  cvDestroyWindow("face");
+  cvDestroyWindow("eye");
 
   return 0;
 }
